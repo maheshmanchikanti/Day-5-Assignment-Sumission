9bc4548bfcde8eadb9b2b808785db97e08f8c1a0
maheshmanchikanti <42872758+maheshmanchikanti@users.noreply.github.com>
2021-02-13 15:26:47 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,17 @@
+def prime():
+    if number > 1:
+        
+        for i in range(2, number):
+            if (number % i) == 0:
+                print(number, "is not a prime number")
+                break
+        else:
+            print(number, "is a prime number")
+
+    else:
+        print(number, "is not a prime number")
+    
+number = int(input("Enter any number: "))
+prime()
+
+
